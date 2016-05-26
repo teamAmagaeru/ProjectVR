6726be78a1b7eb1a9dacab0710cebaad76ef0226
kunisada <governocean@gmail.com>
2016-05-26 23:59:29 +0900

    unityちゃん 仮のアニメーションクラス作成

---
+++ ProjectVR/Assets/Scripts/Object/AnimationObject.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationObject : MonoBehaviour
+{
+	private Animator m_anim = null;
+	// Use this for initialization
+	void Awake ()
+	{
+		m_anim = GetComponent<Animator>();
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	public void Init( Animator anim )
+	{
+		m_anim = anim;
+	}
+
+	public void PlayAnim( string animName )
+	{
+		m_anim.Play( animName );
+	}
+	public void StopAnim()
+	{
+		m_anim.enabled = false;
+	}
+	public void StartAnim()
+	{
+		m_anim.enabled = true;
+	}
+
+}
