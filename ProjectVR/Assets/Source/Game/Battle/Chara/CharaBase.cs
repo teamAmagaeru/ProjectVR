d1ac1d179a919a4cbdc5b100cd9575ee37bf78c6
fukura <jxay_4@yahoo.co.jp>
2016-05-26 23:37:20 +0900

    マネージャ周り作成

---
+++ ProjectVR/Assets/Source/Game/Battle/Chara/CharaBase.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharaBase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    int m_charaId = 0;
+    public int charaId {
+        get { return m_charaId; }
+    }
+}
---
+++ ProjectVR/Assets/Source/Game/Battle/Chara/CharaManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// キャラ管理.
+/// </summary>
+public class CharaManager : ManagerBase{
+    static CharaManager instance
+    {
+        get { return (CharaManager)m_instance; }
+    }
+
+    Dictionary<int, CharaBase> m_charaList = null;
+
+    /// <summary>
+    /// キャラ追加.
+    /// </summary>
+    /// <param name="chara">追加したいキャラ</param>
+    static public void Add(CharaBase chara)
+    {
+        if (instance == null)
+        {
+            return;
+        }
+        instance.m_charaList.Add(chara.charaId,chara);
+    }
+    /// <summary>
+    /// キャラ削除.
+    /// </summary>
+    /// <param name="chara">消したいキャラ</param>
+    static public void Remove(CharaBase chara)
+    {
+        if (instance == null)
+        {
+            return;
+        }
+        instance.m_charaList.Remove(chara.charaId);
+    }
+
+    protected override void Create()
+    {
+        m_charaList = new Dictionary<int, CharaBase>();
+    }
+    protected override void Update()
+    {
+    }
+    protected override void Destroy()
+    {
+    }
+}
