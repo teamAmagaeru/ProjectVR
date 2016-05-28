51422bf915fa6e601c994f8d5b082756b6f8537a
kunisada <governocean@gmail.com>
2016-05-28 15:19:18 +0900

    アニメーション管理クラス

---
+++ ProjectVR/Assets/Source/Game/Object/AnimationObject.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationObject : MonoBehaviour {
+
+	Animator m_anim = null;
+
+	// Use this for initialization
+	void Awake() {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void Init( Animator anim )
+	{
+		m_anim = anim;
+	}
+
+	public void Stop()
+	{
+		m_anim.enabled = false;
+	}
+
+	public void Continue()
+	{
+		m_anim.enabled = true;
+	}
+
+	public void ChangeState( string state )
+	{
+		m_anim.Play( state );
+	}
+
+
+}
