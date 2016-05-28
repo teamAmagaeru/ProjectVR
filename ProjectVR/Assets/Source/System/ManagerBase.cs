d45119f1c3b7f13eb24f0b3092c5162e5320b2ae
kunisada <governocean@gmail.com>
2016-05-28 15:27:08 +0900

    unityちゃん

---
+++ ProjectVR/Assets/Source/System/ManagerBase.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// マネージャの基底.
+/// 派生クラスはstatic関数を実装し、m_instanceのnullチェックを行うこと.
+/// </summary>
+public class ManagerBase {
+
+   protected  static ManagerBase m_instance = null;
+
+   static  public void SysCreate()
+    {
+        if (m_instance == null)
+        {
+            m_instance = new ManagerBase();
+        }
+        m_instance.Create();
+    }
+    static public void SysUpdate()
+    {
+        if (m_instance != null)
+        {
+            m_instance.Update();
+        }
+    }
+    static public void SysDestroy()
+    {
+        if (m_instance != null)
+        {
+            m_instance.Destroy();
+        }
+        m_instance = null;
+    }
+
+    protected virtual void Create()
+    {
+    }
+    protected virtual void Update()
+    {
+    }
+    protected virtual void Destroy()
+    {
+    }
+
+
+}
---
+++ ProjectVR/Assets/Source/System/SystemManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SystemManager : MonoBehaviour {
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+    enum eStep {
+        Init = 0,
+        InitWait,
+        Update
+    }
+    eStep m_step = eStep.Init;
+
+
+	// Update is called once per frame
+	void Update () {
+        switch (m_step)
+        {
+            case eStep.Init:
+                CharaManager.SysCreate();
+                break;
+            case eStep.InitWait:
+                break;
+            case eStep.Update:
+                CharaManager.SysUpdate();
+                break;
+        }
+	}
+}
