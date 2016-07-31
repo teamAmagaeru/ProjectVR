d4526dec4fc4496475ed183f45c20327844e93a5
fukura <jxay_4@yahoo.co.jp>
2016-07-31 18:56:06 +0900

    カメラ回転対応

---
+++ ProjectVR/Assets/Source/System/NotHtcCamera.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NotHtcCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float rotValue = 0.8f;
+
+        if (Input.GetKey(KeyCode.Keypad8))
+        {
+            transform.Rotate(new Vector3(-rotValue, 0.0f, 0.0f));
+        }
+        if (Input.GetKey(KeyCode.Keypad2))
+        {
+            transform.Rotate(new Vector3(rotValue, 0.0f, 0.0f));
+        }
+        if (Input.GetKey(KeyCode.Keypad4))
+        {
+            transform.Rotate(new Vector3(0.0f, -rotValue, 0.0f));
+        }
+        if (Input.GetKey(KeyCode.Keypad6))
+        {
+            transform.Rotate(new Vector3(0.0f, rotValue, 0.0f));
+        }
+
+
+    }
+}
