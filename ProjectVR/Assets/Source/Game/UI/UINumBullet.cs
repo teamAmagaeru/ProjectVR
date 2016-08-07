ac89542946a0c418187e486dfe7718ac03fb67c9
fukura <jxay_4@yahoo.co.jp>
2016-08-08 00:50:06 +0900

    残段数表示作成

--- ProjectVR/Assets/Source/Game/UI/UINumBullet.cs
+++ ProjectVR/Assets/Source/Game/UI/UINumBullet.cs
@@ -16,5 +16,37 @@ public class UINumBullet : MonoBehaviour {
 	}
     public void SetNumBullet(int num)
     {
+        foreach (var n in m_nums)
+        {
+            if (n != null)
+            {
+                GameObject.Destroy(n);
+            }
+        }
+        if (num <= 0)
+        {
+            return;
+        }
+        int t = 0;
+        t = num / 10;
+        m_nums[0] = (GameObject)GameObject.Instantiate(Resources.Load("Prefab/UI/3DNumber/num_" + t));
+        t = num % 10;
+        m_nums[1] = (GameObject)GameObject.Instantiate(Resources.Load("Prefab/UI/3DNumber/num_" + t));
+        foreach (var n in m_nums)
+        {
+            if (n != null)
+            {
+                Utility.SetParent(transform, n.transform);
+                n.transform.Rotate(0.0f, 180.0f, 0.0f);
+            }
+        }
+//        if (num >= 10)
+        {
+            Vector3 pos = Vector3.zero;
+            pos.x = -0.04f;
+            m_nums[0].transform.localPosition = pos;
+            pos.x = 0.04f;
+            m_nums[1].transform.localPosition = pos;
+        }
     }
 }
